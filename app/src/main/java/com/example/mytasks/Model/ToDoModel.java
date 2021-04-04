8d446743e778a1c2b7a9f2d39bcb8013b2213a16
raju <rajshekar@gmail.com>
2021-04-04 18:17:24 +0530

    project

---
+++ app/src/main/java/com/example/mytasks/Model/ToDoModel.java
@@ -0,0 +1,30 @@
+package com.example.mytasks.Model;
+
+public class ToDoModel {
+    private String task;
+    private int id, status;
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
